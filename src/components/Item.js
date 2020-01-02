2965e4030937344c25357c91d6460e125111092b
Ryan Scott <ryan@ryankscott.com>
2020-01-02 20:13:43 +0100

    Adding basic hotkey support for items

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,7 +4,29 @@ import styled from "styled-components";
 import { connect } from "react-redux";
 import { createItem } from "../actions";
 
-const ItemContainer = styled.div``;
+const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 25px;
+  width: 450px;
+  border: 1px solid #ccc;
+  padding: 5px 0px 5px 5px;
+  align-items: center;
+  :focus {
+    background-color: #e5e5e5;
+  }
+`;
+
+// TODO: Conditional styling
+const ItemType = styled.div`
+  color: #ffffff;
+  background-color: #ff9e80;
+  border: 1px solid #ff8e80;
+  margin: 2px 5px 2px 2px;
+  padding: 2px 5px;
+  border-radius: 5px;
+  cursor: pointer;
+`;
 
 class Item extends Component {
   constructor(props) {
@@ -13,13 +35,12 @@ class Item extends Component {
 
   render() {
     return (
-      <li>
-        <p>
-          {this.props.type} - {this.props.text}
-        </p>
-        <p>{this.props.scheduledDate}</p>
-        <p>{this.props.dueDate}</p>
-      </li>
+      <ItemContainer id={this.props.id} tabIndex="0">
+        <ItemType>{this.props.type}</ItemType>
+        {this.props.text}
+        {this.props.scheduledDate}
+        {this.props.dueDate}
+      </ItemContainer>
     );
   }
 }
--- src/containers/ItemList.js
+++ src/containers/ItemList.js
@@ -1,23 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Item from "../components/Item";
+import { HotKeys } from "react-hotkeys";
 
 import { connect } from "react-redux";
+const keyMap = {
+  SCHEDULE: "s",
+  MOVE_UP: "k",
+  MOVE_DOWN: "j"
+};
+const handlers = {
+  SCHEDULE: event => console.log("scheduling!"),
+  MOVE_UP: event =>
+    event.target.previousSibling && event.target.previousSibling.focus(),
+  MOVE_DOWN: event =>
+    event.target.nextSibling && event.target.nextSibling.focus()
+};
 
 const ItemList = ({ items }) => {
   return (
     <ul>
-      {Object.values(items).map(i => {
-        return (
-          <Item
-            key={i.id}
-            type={i.type}
-            text={i.text}
-            scheduledDate={i.scheduledDate}
-            dueDate={i.dueDate}
-          />
-        );
-      })}
+      <HotKeys keyMap={keyMap} handlers={handlers}>
+        {Object.values(items).map(i => {
+          return (
+            <Item
+              key={i.id}
+              type={i.type}
+              text={i.text}
+              scheduledDate={i.scheduledDate}
+              dueDate={i.dueDate}
+            />
+          );
+        })}
+      </HotKeys>
     </ul>
   );
 };
