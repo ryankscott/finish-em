0a9c2a68fb8cc3ea388175882426c8c57c3d5dd7
Ryan Scott <ryan@ryankscott.com>
2020-02-23 20:09:42 +0100

    Adding typescript config

---
+++ src/components/Button.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { theme } from "../theme";
+
+const StyledButton = styled.button`
+  background-color: ${props => props.theme.backgroundColour}
+  color: ${props => props.theme.colour};
+  font-size: ${props => props.theme.fontSizes.xsmall};
+  padding: 2px 5px;
+  height: ${(props: StyledProps) => (props.compact ? "25px" : "30px")};
+  width: ${(props: StyledProps) => (props.compact ? "60px" : "90px")};
+  margin: 2px;
+  border-radius: 5px;
+  border: 1px solid;
+  border-color: ${props => props.theme.borderColour};
+  text-align: center;
+  &:hover {
+    background-color: ${props => props.theme.hoverBackgroundColour};
+    border-color: ${props => props.theme.hoverBackgroundColour};
+    cursor: pointer;
+  }
+`;
+
+type StyledProps = {
+  compact: boolean;
+};
+
+type ButtonProps = {
+  onClick: (x: void) => void;
+  compact: boolean;
+  type: string;
+  children?: React.ReactChild;
+};
+
+const getTheme = (type: string) => {
+  return theme.button[type];
+};
+
+export const Button: React.FunctionComponent<ButtonProps> = props => (
+  <ThemeProvider theme={getTheme(props.type)}>
+    <StyledButton compact={props.compact} onClick={props.onClick}>
+      {props.children}
+    </StyledButton>
+  </ThemeProvider>
+);
---
+++ src/components/Unscheduled.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { useParams, useHistory } from "react-router-dom";
+import { connect } from "react-redux";
+
+import { theme } from "../theme";
+import { Header, Title } from "./Typography";
+import FilteredItemList from "../containers/FilteredItemList";
+
+const UnscheduledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 50px 50px;
+  padding-bottom: 50px;
+  width: 675px;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: baseline;
+`;
+
+function Unscheduled(props) {
+  return (
+    <ThemeProvider theme={theme}>
+      <UnscheduledContainer>
+        <HeaderContainer>
+          <Title> Unscheduled </Title>
+        </HeaderContainer>
+        <FilteredItemList filter="SHOW_NOT_SCHEDULED" sortCriteria="DUE" />
+      </UnscheduledContainer>
+    </ThemeProvider>
+  );
+}
+
+const mapStateToProps = state => ({
+  items: state.items
+});
+const mapDispatchToProps = dispatch => ({});
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Unscheduled);
