4c464a5e81c344f6f6e00ffc073bbf0bdd4cfb55
Ryan Scott <ryan@ryankscott.com>
2020-03-28 14:45:47 +0100

    Add missing timestamp to Item

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -12,6 +12,7 @@ export interface ItemType {
   scheduledDate: string;
   lastUpdatedAt: string;
   completedAt: string;
+  createdAt: string;
   deletedAt: string;
   repeat: string;
   hidden: boolean;
