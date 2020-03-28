230bd6b1331cc2f5fbe17aea0893a16035b0efc1
Ryan Scott <ryan@ryankscott.com>
2020-03-28 14:46:04 +0100

    Change icons for collapsing / expanding

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -135,7 +135,6 @@ export const addIcon = (width, height) => (
 export const expandedIcon = (width, height) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={width ? width : "18"}
     height={height ? height : "18"}
     viewBox="0 0 24 24"
     fill="none"
@@ -151,7 +150,6 @@ export const expandedIcon = (width, height) => (
 export const collapsedIcon = (width, height) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={width ? width : "18"}
     height={height ? height : "18"}
     viewBox="0 0 24 24"
     fill="none"
@@ -160,7 +158,7 @@ export const collapsedIcon = (width, height) => (
     strokeLinecap="round"
     strokeLinejoin="round"
   >
-    <polyline points="18 15 12 9 6 15"></polyline>
+    <polyline points="9 18 15 12 9 6"></polyline>
   </svg>
 );
 
