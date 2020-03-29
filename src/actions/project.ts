9187826575cc2d423d97bbb192a9701733b3cf2e
Ryan Scott <ryan@ryankscott.com>
2020-03-29 16:16:44 +0200

    Adding interfaces for project actions

--- src/actions/project.ts
+++ src/actions/project.ts
@@ -1,8 +1,16 @@
-import { Uuid } from "@typed/uuid";
+import { Uuid, uuid } from "@typed/uuid";
+import { CreateProjectDialogProps } from "../components/CreateProjectDialog";
+import { UPDATE_ITEM_DESCRIPTION, CREATE_ITEM } from "./item";
 export const CREATE_PROJECT = "CREATE_PROJECT";
 export const DELETE_PROJECT = "DELETE_PROJECT";
 export const UPDATE_PROJECT_DESCRIPTION = "UPDATE_PROJECT_DESCRIPTION";
 
+export interface CreateProjectAction {
+  type: typeof CREATE_ITEM;
+  id: Uuid;
+  name: string;
+  description: string;
+}
 export function createProject(id: Uuid, name: string, description: string) {
   return {
     type: CREATE_PROJECT,
@@ -12,13 +20,21 @@ export function createProject(id: Uuid, name: string, description: string) {
   };
 }
 
+export interface DeleteProjectAction {
+  type: typeof DELETE_PROJECT;
+  id: Uuid;
+}
 export function deleteProject(id: Uuid) {
   return {
     type: DELETE_PROJECT,
     id: id
   };
 }
-
+export interface UpdateProjectDescriptionAction {
+  type: typeof UPDATE_ITEM_DESCRIPTION;
+  id: Uuid;
+  description: string;
+}
 export function updateProjectDescription(id: Uuid, description: string) {
   return {
     type: UPDATE_PROJECT_DESCRIPTION,
@@ -26,3 +42,8 @@ export function updateProjectDescription(id: Uuid, description: string) {
     description: description
   };
 }
+
+export type ProjectActions =
+  | CreateProjectAction
+  | DeleteProjectAction
+  | UpdateProjectDescriptionAction;
