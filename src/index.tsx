e7bd4c6ad9d921effc2c0f06c05847bb22a3dd25
Raghuvir Kasturi <raghuvir.kasturi@gmail.com>
2020-04-04 10:11:01 +1300

    Migrate

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,12 @@ import { store, persistor } from "./store/";
 import { PersistGate } from "redux-persist/integration/react";
 
 const mainRoot = document.getElementById("mainRoot");
+
 ReactDOM.render(
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <ViewManager />
-    </PersistGate>
-  </Provider>,
-  mainRoot
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+            <ViewManager />
+        </PersistGate>
+    </Provider>,
+    mainRoot
 );
