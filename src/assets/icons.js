1586747cceb0d4e2ff727c2818989cb0f2004119
Ryan Scott <ryan@ryankscott.com>
2020-04-11 22:02:22 +0200

    Fix icon sizes and add two new types

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -7,8 +7,8 @@ export const repeatIcon = (width, height) => (
   <svg
     key="repeatIcon"
     xmlns="http://www.w3.org/2000/svg"
-    width={width ? width : "12"}
-    height={height ? height : "12"}
+    width={width ? width : "16"}
+    height={height ? height : "16"}
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
@@ -27,8 +27,8 @@ export const dueIcon = (width, height) => (
   <svg
     key="dueIcon"
     xmlns="http://www.w3.org/2000/svg"
-    width={width ? width : "12"}
-    height={height ? height : "12"}
+    width={width ? width : "16"}
+    height={height ? height : "16"}
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
@@ -46,8 +46,8 @@ export const scheduledIcon = (width, height) => (
   <svg
     key="scheduledIcon"
     xmlns="http://www.w3.org/2000/svg"
-    width={width ? width : "12"}
-    height={height ? height : "12"}
+    width={width ? width : "16"}
+    height={height ? height : "16"}
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
@@ -185,8 +185,8 @@ export const helpIcon = (width, height) => (
 export const closeIcon = (height, width) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={width ? width : "12"}
-    height={height ? height : "12"}
+    width={width ? width : "16"}
+    height={height ? height : "16"}
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
@@ -202,8 +202,8 @@ export const closeIcon = (height, width) => (
 export const sortIcon = (height, width) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width={width ? width : "12"}
-    height={height ? height : "12"}
+    width={width ? width : "16"}
+    height={height ? height : "16"}
     viewBox="0 0 24 24"
     fill="none"
     stroke="currentColor"
@@ -215,3 +215,55 @@ export const sortIcon = (height, width) => (
     <polyline points="19 12 12 19 5 12"></polyline>
   </svg>
 );
+
+
+export const trashIcon = (height, width) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width ? width : "16"}
+    height={height ? height : "16"}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.75"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <polyline points="3 6 5 6 21 6"></polyline>
+    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+    <line x1="10" y1="11" x2="10" y2="17"></line>
+    <line x1="14" y1="11" x2="14" y2="17"></line>
+  </svg>
+);
+
+export const showIcon = (height, width) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width ? width : "16"}
+    height={height ? height : "16"}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+    <circle cx="12" cy="12" r="3"></circle>
+  </svg >
+);
+
+export const hideIcon = (height, width) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={width ? width : "16"}
+    height={height ? height : "16"}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round">
+    ><path d="M17.94 17.94A10.07 10.07 0 0 1 12 20c-7 0-11-8-11-8a18.45 18.45 0 0 1 5.06-5.94M9.9 4.24A9.12 9.12 0 0 1 12 4c7 0 11 8 11 8a18.5 18.5 0 0 1-2.16 3.19m-6.72-1.07a3 3 0 1 1-4.24-4.24"></path>
+    <line x1="1" y1="1" x2="23" y2="23"></line>
+  </svg>
+)
