7fe0aafe509087c788c0ca519db56139f7df139a
Ryan Scott <ryan@ryankscott.com>
2020-04-15 19:37:07 +0200

    Fixes #47

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -5,15 +5,15 @@ import ReactTooltip from 'react-tooltip'
 import { Paragraph } from './Typography'
 
 interface TooltipProps {
-  id: string
-  text: string
+    id: string
+    text: string
 }
 export const Tooltip = (props: TooltipProps): ReactElement => {
-  return (
-    <ThemeProvider theme={theme}>
-      <ReactTooltip id={props.id} type="dark" effect="float" place="right">
-        <Paragraph invert>{props.text}</Paragraph>
-      </ReactTooltip>
-    </ThemeProvider>
-  )
+    return (
+        <ThemeProvider theme={theme}>
+            <ReactTooltip id={props.id} type="dark" effect="float" place="top">
+                <Paragraph invert>{props.text}</Paragraph>
+            </ReactTooltip>
+        </ThemeProvider>
+    )
 }
