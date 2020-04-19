37200bb09da8e610bb669e5ed1ecc2ac7f683e69
Ryan Scott <ryan@ryankscott.com>
2020-04-19 13:38:17 +0200

    Update trash sweep icon

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -278,6 +278,8 @@ export const trashSweepIcon = (width, height) => (
     >
         <polyline points="3 6 5 6 21 6"> </polyline>
         <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
+        <line x1="10" y1="11" x2="10" y2="17"></line>
+        <line x1="14" y1="11" x2="14" y2="17"></line>
         <line x1="25" y1="8" x2="32" y2="8"></line>
         <line x1="25" y1="14" x2="30" y2="14"></line>
         <line x1="25" y1="20" x2="28" y2="20"></line>
