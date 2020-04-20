a59bab3543e530b9e193ce3de7ddae3cc42e67b6
Ryan Scott <ryan@ryankscott.com>
2020-04-20 19:45:25 +0200

    Make repeat icon thinner

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -12,7 +12,7 @@ export const repeatIcon = (width, height) => (
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        strokeWidth="2"
+        strokeWidth="1.75"
         strokeLinecap="round"
         strokeLinejoin="round"
     >
