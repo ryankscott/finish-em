fc069f14db2fcc3e18ee39b67c782f7fe20a8a39
Ryan Scott <ryan@ryankscott.com>
2020-05-29 19:06:46 +0200

    Fix #146 - Break completed into completed today, this week, this month  etc.

--- src/components/Completed.tsx
+++ src/components/Completed.tsx
@@ -5,6 +5,8 @@ import { theme } from '../theme'
 import { Title } from './Typography'
 import FilteredItemList, { FilterEnum } from '../containers/FilteredItemList'
 import { CompletedContainer, HeaderContainer } from './styled/Completed'
+import { isToday, parseISO, isThisMonth, isThisWeek } from 'date-fns'
+import { ItemType } from '../interfaces'
 
 const Completed = (): ReactElement => (
     <ThemeProvider theme={theme}>
@@ -18,6 +20,63 @@ const Completed = (): ReactElement => (
                     filter: FilterEnum.ShowCompleted,
                 }}
                 isFilterable={false}
+            />
+            <FilteredItemList
+                listName="Completed today"
+                filter={{
+                    type: 'custom',
+                    filter: (i: ItemType) => {
+                        return (
+                            i.completed == true &&
+                            isToday(parseISO(i.completedAt))
+                        )
+                    },
+                }}
+                isFilterable={true}
+            />
+            <FilteredItemList
+                listName="Completed this week"
+                filter={{
+                    type: 'custom',
+                    filter: (i: ItemType) => {
+                        return (
+                            i.completed == true &&
+                            !isToday(parseISO(i.completedAt)) &&
+                            isThisWeek(parseISO(i.completedAt))
+                        )
+                    },
+                }}
+                isFilterable={true}
+            />
+            <FilteredItemList
+                listName="Completed this month"
+                filter={{
+                    type: 'custom',
+                    filter: (i: ItemType) => {
+                        return (
+                            i.completed == true &&
+                            !isToday(parseISO(i.completedAt)) &&
+                            !isThisWeek(parseISO(i.completedAt)) &&
+                            isThisMonth(parseISO(i.completedAt))
+                        )
+                    },
+                }}
+                isFilterable={true}
+            />
+            <FilteredItemList
+                listName="Older"
+                filter={{
+                    type: 'custom',
+                    filter: (i: ItemType) => {
+                        return (
+                            i.completed == true &&
+                            !isToday(parseISO(i.completedAt)) &&
+                            !isThisWeek(parseISO(i.completedAt)) &&
+                            !isThisMonth(parseISO(i.completedAt))
+                        )
+                    },
+                }}
+                isFilterable={true}
             />
         </CompletedContainer>
     </ThemeProvider>
