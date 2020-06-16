b7d735a97eadd554e06d67776ce7f36543661e93
Ryan Scott <ryan@ryankscott.com>
2020-06-16 20:33:43 +0200

    Use npm version of filtrex, and fix build scripts

--- src/selectors/item.ts
+++ src/selectors/item.ts
@@ -1,7 +1,7 @@
 import { createSelector } from 'reselect'
 import { Item, ItemType } from '../interfaces'
 import { filterItems, filtrexOptions } from '../utils'
-import { compileExpression } from '../utils/filtrex.js'
+import { compileExpression } from 'filtrex'
 
 export const getRenderingStrategy = (state, props) => {
     return props.renderingStrategy
