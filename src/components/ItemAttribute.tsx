cae4f655cd0157edb9105c0ea7449d133e3bd0f7
Ryan Scott <ryan@ryankscott.com>
2020-06-30 20:32:14 +0200

    Fix Items need to support markdown for their parent items #194

--- src/components/ItemAttribute.tsx
+++ src/components/ItemAttribute.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement } from 'react'
 import { AttributeContainer, AttributeIcon, AttributeText } from './styled/ItemAttribute'
 import { Icons } from '../assets/icons'
+import marked from 'marked'
 
 interface OwnProps {
     type: 'repeat' | 'due' | 'scheduled' | 'subtask'
@@ -13,7 +14,7 @@ export const ItemAttribute = (props: ItemAttributeProps): ReactElement => {
     return (
         <AttributeContainer completed={props.completed}>
             <AttributeIcon> {Icons[props.type](14, 14)}</AttributeIcon>
-            <AttributeText>{props.text}</AttributeText>
+            <AttributeText dangerouslySetInnerHTML={{ __html: marked(props.text) }}></AttributeText>
         </AttributeContainer>
     )
 }
