2749c7f42c4a3978a86681a619791d461ca2c3c5
Ryan Scott <ryan@ryankscott.com>
2020-07-08 21:27:37 +0200

    Fix Introduce some sweet styling on filter bars #204

--- src/components/styled/FilteredItemList.ts
+++ src/components/styled/FilteredItemList.ts
@@ -7,8 +7,10 @@ export const HeaderBar = styled.div`
     display: grid;
     align-items: center;
     width: 100%;
-    padding: 5px;
-    border-radius: 5px;
+    padding: 12px 5px;
+    margin-top: 0px;
+    border-radius: 5px 5px 0px 0px;
+    box-shadow: ${(props) => '0px 1px 4px ' + props.theme.colours.borderColour};
     grid-template-rows: 40px;
     grid-template-columns: 30px 200px auto;
     grid-template-areas: 'hide header filterBar';
@@ -66,8 +68,9 @@ export const ListItemCount = styled(Paragraph)`
 `
 
 export const Container = styled.div`
-    margin: 10px 0px;
-    padding: 10px 0px;
+    margin: 0px;
+    margin-bottom: 10px;
+    padding: 0px;
     width: 100%;
     border-radius: 5px;
     box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.1);
