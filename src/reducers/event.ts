9d2a9e01c2dac8e78340bdacf0a17e100a1b1758
Ryan Scott <ryan@ryankscott.com>
2020-08-23 22:05:03 +0200

    Adding event types

---
+++ src/reducers/event.ts
@@ -0,0 +1,17 @@
+import * as event from '../actions/event'
+import { Events } from '../interfaces/event'
+import produce from 'immer'
+
+const initialState: Events = []
+
+export const eventReducer = produce(
+    (draftState: Events = initialState, action: event.EventActions): Events => {
+        switch (action.type) {
+            case event.CREATE_EVENT:
+                break
+
+            default:
+                return draftState
+        }
+    },
+)
