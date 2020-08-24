5938c24d63eab83baf352b34fefe4b3335a9e014
Ryan Scott <ryan@ryankscott.com>
2020-08-24 18:52:10 +0200

    Properly implement readOnly filteredItemLists

--- src/components/FilteredItemList.tsx
+++ src/components/FilteredItemList.tsx
@@ -369,21 +369,23 @@ function FilteredItemList(props: FilteredItemListProps): ReactElement {
                         )}
                     </FilterBar>
                     <div style={{ gridArea: 'more' }}>
-                        <MoreDropdown
-                            options={[
-                                {
-                                    icon: 'trash',
-                                    label: 'Delete Component',
-                                    onClick: (e: React.MouseEvent) =>
-                                        props.deleteComponent(props.id),
-                                },
-                                {
-                                    icon: 'edit',
-                                    label: 'Edit Component',
-                                    onClick: (e: React.MouseEvent) => setShowEditDialog(true),
-                                },
-                            ]}
-                        />
+                        {!props.readOnly && (
+                            <MoreDropdown
+                                options={[
+                                    {
+                                        icon: 'trash',
+                                        label: 'Delete Component',
+                                        onClick: (e: React.MouseEvent) =>
+                                            props.deleteComponent(props.id),
+                                    },
+                                    {
+                                        icon: 'edit',
+                                        label: 'Edit Component',
+                                        onClick: (e: React.MouseEvent) => setShowEditDialog(true),
+                                    },
+                                ]}
+                            />
+                        )}
                     </div>
                     {showEditDialog && (
                         <FilteredItemDialog
