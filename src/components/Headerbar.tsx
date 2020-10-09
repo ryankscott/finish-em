da79b35bebb9ae66dd37ec71014029dc3ae8e4e9
Ryan Scott <ryan@ryankscott.com>
2020-10-09 12:00:22 +0200

    Adding icon to headerbar

--- src/components/Headerbar.tsx
+++ src/components/Headerbar.tsx
@@ -11,118 +11,121 @@ import { removeItemTypeFromString, markdownLinkRegex, markdownBasicRegex } from 
 import { useHistory } from 'react-router'
 import { Project, Projects } from '../interfaces/project'
 import { Items, Item } from '../interfaces'
-import { ShortcutIcon, SelectContainer, FeedbackIcon, Container } from './styled/Headerbar'
+import {
+  ShortcutIcon,
+  IconContainer,
+  SelectContainer,
+  FeedbackIcon,
+  Container,
+} from './styled/Headerbar'
+import { Icons } from '../assets/icons'
 
 type OptionType = { label: string; value: () => void }
 
 interface OwnProps {
-    searchRef: React.RefObject<HTMLSelectElement>
+  searchRef: React.RefObject<HTMLSelectElement>
 }
 interface StateProps {
-    theme: string
-    projects: Projects
-    items: Items
+  theme: string
+  projects: Projects
+  items: Items
 }
 
 interface DispatchProps {
-    setActiveItem: (id: string) => void
+  setActiveItem: (id: string) => void
 }
 
 type HeaderbarProps = DispatchProps & StateProps & OwnProps
 
 const Headerbar = (props: HeaderbarProps): ReactElement => {
-    const history = useHistory()
-    const generateOptions = (projects: Project, items: Item): GroupType<OptionType>[] => {
-        const itemOptions = Object.values(items).map((i) => {
-            return {
-                label: removeItemTypeFromString(i.text)
-                    .replace(markdownLinkRegex, '$1')
-                    .replace(markdownBasicRegex, '$1'),
-                value: () => props.setActiveItem(i.id),
-            }
-        })
-        const projectOptions = Object.values(projects).map((p) => {
-            return {
-                label: p.name,
-                value: () => history.push(`/projects/${p.id}`),
-            }
-        })
+  const history = useHistory()
+  const generateOptions = (projects: Project, items: Item): GroupType<OptionType>[] => {
+    const itemOptions = Object.values(items).map((i) => {
+      return {
+        label: removeItemTypeFromString(i.text)
+          .replace(markdownLinkRegex, '$1')
+          .replace(markdownBasicRegex, '$1'),
+        value: () => props.setActiveItem(i.id),
+      }
+    })
+    const projectOptions = Object.values(projects).map((p) => {
+      return {
+        label: p.name,
+        value: () => history.push(`/projects/${p.id}`),
+      }
+    })
 
-        return [
-            { label: 'Items', options: itemOptions },
-            { label: 'Projects', options: projectOptions },
-        ]
-    }
+    return [
+      { label: 'Items', options: itemOptions },
+      { label: 'Projects', options: projectOptions },
+    ]
+  }
 
-    return (
-        <Container>
-            <ThemeProvider theme={themes[props.theme]}>
-                <SelectContainer>
-                    <Select
-                        controlShouldRenderValue={false}
-                        escapeClearsValue={true}
-                        ref={props.searchRef}
-                        width="400px"
-                        height="25px"
-                        placeholder="Search for items..."
-                        onChange={(selected) => {
-                            selected.value()
-                        }}
-                        options={generateOptions(props.projects.projects, props.items.items)}
-                        styles={selectStyles({
-                            fontSize: 'xsmall',
-                            theme: themes[props.theme],
-                            width: '400px',
-                            backgroundColour: lighten(
-                                0.2,
-                                themes[props.theme].colours.headerBackgroundColour,
-                            ),
-                        })}
-                    />
-                </SelectContainer>
-                <FeedbackIcon>
-                    <Button
-                        dataFor="feedback-button"
-                        type="subtle"
-                        icon="feedback"
-                        iconSize="20px"
-                        iconColour={themes[props.theme].colours.altTextColour}
-                        onClick={() =>
-                            window.open('https://github.com/ryankscott/finish-em/issues/new/')
-                        }
-                    ></Button>
-                    <Tooltip id="feedback-button" text={'Give feedback'}></Tooltip>
-                </FeedbackIcon>
-                <ShortcutIcon id="shortcut-icon">
-                    <Button
-                        dataFor="shortcut-button"
-                        id="shortcut-button"
-                        type="subtle"
-                        icon="help"
-                        iconSize="20px"
-                        iconColour={themes[props.theme].colours.altTextColour}
-                        onClick={() => {
-                            history.push('/help/')
-                        }}
-                    ></Button>
-                    <Tooltip id="shortcut-button" text={'Show shortcuts'}></Tooltip>
-                </ShortcutIcon>
-            </ThemeProvider>
-        </Container>
-    )
+  return (
+    <Container>
+      <ThemeProvider theme={themes[props.theme]}>
+        <IconContainer>{Icons['finish_em'](36, 36)}</IconContainer>
+        <SelectContainer>
+          <Select
+            controlShouldRenderValue={false}
+            escapeClearsValue={true}
+            ref={props.searchRef}
+            width="400px"
+            height="25px"
+            placeholder="Search for items..."
+            onChange={(selected) => {
+              selected.value()
+            }}
+            options={generateOptions(props.projects.projects, props.items.items)}
+            styles={selectStyles({
+              fontSize: 'xsmall',
+              theme: themes[props.theme],
+              width: '400px',
+              backgroundColour: lighten(0.2, themes[props.theme].colours.headerBackgroundColour),
+            })}
+          />
+        </SelectContainer>
+        <FeedbackIcon>
+          <Button
+            dataFor="feedback-button"
+            type="subtle"
+            icon="feedback"
+            iconSize="20px"
+            iconColour={themes[props.theme].colours.altTextColour}
+            onClick={() => window.open('https://github.com/ryankscott/finish-em/issues/new/')}
+          ></Button>
+          <Tooltip id="feedback-button" text={'Give feedback'}></Tooltip>
+        </FeedbackIcon>
+        <ShortcutIcon id="shortcut-icon">
+          <Button
+            dataFor="shortcut-button"
+            id="shortcut-button"
+            type="subtle"
+            icon="help"
+            iconSize="20px"
+            iconColour={themes[props.theme].colours.altTextColour}
+            onClick={() => {
+              history.push('/help/')
+            }}
+          ></Button>
+          <Tooltip id="shortcut-button" text={'Show shortcuts'}></Tooltip>
+        </ShortcutIcon>
+      </ThemeProvider>
+    </Container>
+  )
 }
 
 const mapStateToProps = (state): StateProps => ({
-    projects: state.projects,
-    items: state.items,
-    theme: state.ui.theme,
+  projects: state.projects,
+  items: state.items,
+  theme: state.ui.theme,
 })
 
 const mapDispatchToProps = (dispatch): DispatchProps => ({
-    setActiveItem: (id: string) => {
-        dispatch(showFocusbar())
-        dispatch(setActiveItem(id))
-    },
+  setActiveItem: (id: string) => {
+    dispatch(showFocusbar())
+    dispatch(setActiveItem(id))
+  },
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Headerbar)
