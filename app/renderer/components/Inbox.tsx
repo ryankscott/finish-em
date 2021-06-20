97f150c1a52ed5e5184a924c09145ca5b24ad845
Ryan Scott <ryan@ryankscott.com>
2021-06-20 16:20:58 +0300

    Fix #391 - Inbox padding

--- app/renderer/components/Inbox.tsx
+++ app/renderer/components/Inbox.tsx
@@ -5,41 +5,44 @@ import ItemCreator from './ItemCreator'
 import ViewHeader from './ViewHeader'
 import { Flex } from '@chakra-ui/react'
 
+import { Page } from './Page'
 const Inbox = (): ReactElement => {
   return (
-    <Flex margin={5} marginTop={12} padding={5} width="100%" direction="column" maxW="800px">
-      <ViewHeader
-        componentKey="42c6cea5-785f-4418-bd0f-5f4d388f4496"
-        name={'Inbox'}
-        icon={'inbox'}
-        readOnly={true}
-      />
-      <ItemCreator
-        buttonText="Add Item"
-        initiallyExpanded={true}
-        shouldCloseOnSubmit={false}
-        projectKey={'0'}
-      />
-      <Flex marginTop="2">
-        <FilteredItemList
-          componentKey="42c6cea5-785f-4418-bd0f-5f4d388f4497"
-          isFilterable={true}
-          listName="Inbox"
-          filter={JSON.stringify({
-            text: 'project = "Inbox"',
-            value: [
-              { category: 'projectKey', operator: '=', value: '0' },
-              { conditionType: 'AND', category: 'areaKey', operator: 'is', value: 'null' },
-              { conditionType: 'AND', category: 'deleted', operator: '=', value: 'false' },
-              { conditionType: 'AND', category: 'completed', operator: '=', value: 'false' },
-            ],
-          })}
-          flattenSubtasks={false}
+    <Page>
+      <Flex direction="column" m={5} p={5}>
+        <ViewHeader
+          componentKey="42c6cea5-785f-4418-bd0f-5f4d388f4496"
+          name={'Inbox'}
+          icon={'inbox'}
           readOnly={true}
-          hiddenIcons={[ItemIcons.Project]}
         />
+        <ItemCreator
+          buttonText="Add Item"
+          initiallyExpanded={true}
+          shouldCloseOnSubmit={false}
+          projectKey={'0'}
+        />
+        <Flex marginTop="2">
+          <FilteredItemList
+            componentKey="42c6cea5-785f-4418-bd0f-5f4d388f4497"
+            isFilterable={true}
+            listName="Inbox"
+            filter={JSON.stringify({
+              text: 'project = "Inbox"',
+              value: [
+                { category: 'projectKey', operator: '=', value: '0' },
+                { conditionType: 'AND', category: 'areaKey', operator: 'is', value: 'null' },
+                { conditionType: 'AND', category: 'deleted', operator: '=', value: 'false' },
+                { conditionType: 'AND', category: 'completed', operator: '=', value: 'false' },
+              ],
+            })}
+            flattenSubtasks={false}
+            readOnly={true}
+            hiddenIcons={[ItemIcons.Project]}
+          />
+        </Flex>
       </Flex>
-    </Flex>
+    </Page>
   )
 }
 
