b57639c0fb6faca9f7527ebe859b83b7708e127c
Ryan Scott <ryan@ryankscott.com>
2022-10-26 08:59:29 +0200

    Add missing component

---
+++ src/renderer/components/DragHandle.tsx
@@ -0,0 +1,31 @@
+import { Flex, Icon } from '@chakra-ui/react';
+import { DraggableProvidedDragHandleProps } from 'react-beautiful-dnd';
+import { Icons } from 'renderer/assets/icons';
+
+interface DragHandleProps {
+  dragHandleProps: DraggableProvidedDragHandleProps | undefined;
+}
+const DragHandle = ({ dragHandleProps }: DragHandleProps) => {
+  return (
+    <Flex
+      position="absolute"
+      direction="row"
+      alignItems="center"
+      justifyContent="center"
+      h={6}
+      w="200px"
+      left="calc(50% - 100px)"
+      zIndex={100}
+      opacity={0}
+      _hover={{
+        opacity: 1,
+      }}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...dragHandleProps}
+    >
+      <Icon as={Icons.drag} opacity={0.8} />
+    </Flex>
+  );
+};
+
+export default DragHandle;
